7cb0f98908c958db79be53855076874241e19529
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-09-07 18:44:13 +0200

    Displaying the main window

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 """main.py represents the core application and handles the main application window."""
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QAction
+
+
+class MainAppWindow(QMainWindow):
+    def __init__(self) -> None:
+        """An instance of the main application window."""
+
+        # Initialize the parent QMainWindow object
+        super().__init__()
+
+        self.setWindowTitle("MetroNetSim")
+
+        # Set the menu bar
+        menu_bar = self.menuBar()
+
+        # Create the "File" menu
+        file_menu = menu_bar.addMenu("File")
+
+        # Set the actions of the "File" menu
+        open_network_action = QAction("Open network database...", self)
+
+        # Add the actions to the "File" menu
+        file_menu.addAction(open_network_action)
+
+
+
+app = QApplication([])
+window = MainAppWindow()
+
+window.show()
+app.exec()
