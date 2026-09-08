720efd343cd646e2bf2edc37fcf0a5ea01590bf2
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-09-08 09:24:42 +0200

    Using an internal graph for the Network class

--- network.py
+++ network.py
@@ -1,18 +1,19 @@
 """networks.py provides a Network class representing a metro graph."""
-import networkx
+import networkx as nx
 import sqlite3
 
 
-class Network(networkx.Graph):
+class Network:
     def __init__(self, file:str):
         """A network graph instance.
         - file: the database file (.db) used by that network."""
         
-        # Initialize the parent Graph object
-        super().__init__()
 
         self.file = file
         print("Network file: ", self.file)
 
         self.connection = sqlite3.Connection(self.file)
         self.cursor = sqlite3.Cursor(self.connection)
+
+        # Create the internal graph
+        self.graph = nx.Graph()
